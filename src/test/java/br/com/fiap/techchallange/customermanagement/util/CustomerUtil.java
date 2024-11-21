98e5d2db0c26ca225431cde0b9db53684f31856a
Ingrid Queiroz <queirozingrid.c@gmail.com>
2024-11-21 19:27:28 -0300

    Add Integration Tests

--- src/test/java/br/com/fiap/techchallange/customermanagement/util/CustomerUtil.java
+++ src/test/java/br/com/fiap/techchallange/customermanagement/util/CustomerUtil.java
@@ -1,7 +1,5 @@
 package br.com.fiap.techchallange.customermanagement.util;
-
 import br.com.fiap.techchallange.customermanagement.core.usecase.dto.customer.InputDataCustomerDTO;
-import br.com.fiap.techchallange.customermanagement.core.usecase.dto.customer.OutputDataCustomerDTO;
 
 public class CustomerUtil {
     public static InputDataCustomerDTO generateCustomer(String cpf, String name, String email) {
